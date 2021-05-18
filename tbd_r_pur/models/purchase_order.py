cb07e0f533989418ba2a9bcca5b6225a7ad7bc18
Alexandre Rosset <alexandre.rosset@tbdgroup.com>
2021-05-18 16:41:25 +0200

    #23821 Fix  duplicate price on view pivot

--- tbd_r_pur/models/purchase_order.py
+++ tbd_r_pur/models/purchase_order.py
@@ -16,9 +16,8 @@ class PurchaseReportCustom(models.Model):
         return super(PurchaseReportCustom, self)._select() + ", am.invoice_date_due as due_date"
 
     def _from(self):
-        return super(PurchaseReportCustom, self)._from() + " left join account_move_purchase_order_rel ampor on (l.order_id = ampor.purchase_order_id) \
-                left join account_move am on (ampor.account_move_id = am.id) \
-                left join account_move_line aml on (aml.product_id = am.product_id)"
+        return super(PurchaseReportCustom, self)._from() + " left join account_move_purchase_order_rel ampor on (purchase_order.id = ampor.purchase_order_id) \
+                left join account_move am on (ampor.account_move_id = am.id) "
 
     def _group_by(self):
         return super(PurchaseReportCustom, self)._group_by() + ", am.invoice_date_due"
